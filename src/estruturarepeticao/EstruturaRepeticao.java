6f30be17b856a4ce2139750072efeabf31f442a4
pkovbin <reginaazzi@hotmail.com>
2022-02-08 21:38:12 -0300

    first commit

---
+++ src/estruturarepeticao/EstruturaRepeticao.java
@@ -0,0 +1,14 @@
+package estruturarepeticao;
+
+public class EstruturaRepeticao {
+    public static void main(String[] args) {
+        int cc = 0;
+        while (cc < 10) {
+            System.out.println("Cambalhota" + cc);
+            cc++;
+        }
+    }
+}
+
+
+
